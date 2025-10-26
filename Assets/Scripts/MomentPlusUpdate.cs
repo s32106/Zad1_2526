adb23c708aa599c37207f5e7756cace4ec942832
s32106 <s32106@pjwstk.edu.pl>
2025-10-26 16:43:41 +0100

    blop

---
+++ Assets/Scripts/MomentPlusUpdate.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerControllerUpdate : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float jumpForce = 100;
+    private float moveInput = 0;
+    public bool isJump = false;
+
+    public Rigidbody2D rb;
+    public SpriteRenderer spriteRenderer;
+    public GroundChecker groundChecker;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isJump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput * moveSpeed * Time.fixedDeltaTime, rb.velocity.y);
+        if (isJump && groundChecker.isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+}
